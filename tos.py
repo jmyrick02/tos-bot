853e7413f2f4e6e8928ac8f37570f86319c23ce4
jamster02 <63320517+jamster02@users.noreply.github.com>
2020-06-15 14:38:59 -0500

    Update tos.py

--- tos.py
+++ tos.py
@@ -1,7 +1,7 @@
 import discord, datetime, asyncio, gspread # TODO a death system
-from discord.ext import tasks, commands # TODO an automatic voting system
+from discord.ext import tasks, commands  # TODO an automatic voting system
 from oauth2client.service_account import ServiceAccountCredentials # TODO add an option for random assignment of roles from a role list. eg. you put Random as the role name when adding the player and then when the game starts it uses the supplied (optional) role list to randomly assign roles to players, taking into account which roles were manually assigned
-from enum import Enum # TODO add system for custom roles
+from enum import Enum  # TODO add system for custom roles
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
@@ -61,21 +61,18 @@ class SalemRole(Enum):
 
 class Player:
     """The class for players"""
+
     def __init__(self, user_id, personal_channel_id, role):
         self.user_id = user_id
         self.personal_channel_id = personal_channel_id
         self.role = role
         self.alive = True
 
-    #INVESTIGATOR
-    def investigate(self, target):
-        if self.role == SalemRole.INVESTIGATOR:
-            target_role = target.role
-
 class Game:
     """The main game class"""
+
     def __init__(self, bot, guild_id, in_progress, game_channel_id, player_role_id, transition_times, players):
-        self.day = .5 # 1 = day 1, 1.5 = night 1 etc.
+        self.day = .5  # 1 = day 1, 1.5 = night 1 etc.
         self.bot = bot
         self.game_channel_id = game_channel_id
         self.guild_id = guild_id
@@ -86,7 +83,7 @@ class Game:
 
     async def progress_time(self):
         self.day += 0.5
-        if int(self.day) == self.day: # day time
+        if int(self.day) == self.day:  # day time
             await self.bot.get_channel(self.game_channel_id).send(f'**DAY {int(self.day)}**')
             await self.bot.get_channel(self.game_channel_id).set_permissions(self.bot.get_guild(self.guild_id).get_role(self.player_role_id), send_messages=True)
         else:
@@ -114,7 +111,7 @@ class Game:
 @bot.event
 async def on_ready():
     print('Logged on as', bot.user)
-    
+
     update.start()
     for guild in bot.guilds:
         load(guild.id)
@@ -127,20 +124,27 @@ async def setup_game(ctx, player_role: discord.Role, hour1: int, minute1: int, h
         game_channel_id = ctx.channel.id
         player_role_id = player_role.id
         transition_times = [[hour1, minute1], [hour2, minute2]]
-        games[guild_id] = Game(bot, guild_id, False, game_channel_id, player_role_id, transition_times, [])
+        games[guild_id] = Game(
+            bot, guild_id, False, game_channel_id, player_role_id, transition_times, [])
         save(guild_id)
 
         await ctx.send('The game instance is set up. Use the command "delete_game" to delete this game instance and to be able to restart. Finish set up using the command "add_player" in each player\'s respective personal channel. Use the command "game_info" to review info about this instance. Use the command "start_game" to begin this instance.')
     else:
-       await ctx.send('A game instance already exists. Delete this instance with the command "delete_game" to be able to create a new one.') 
+        await ctx.send('A game instance already exists. Delete this instance with the command "delete_game" to be able to create a new one.')
     await ctx.message.delete()
 
 @bot.command(brief='Adds a player and their role to the game instance.', description='List of roles (ignore the numbers): https://drive.google.com/file/d/1MdQJCUaKRM_jPIPN2NU3IcY0Vtp2fnI3/view?usp=sharing')
 @commands.has_permissions(administrator=True)
-async def add_player(ctx, nick, role):
+async def add_player(ctx, nick: str, role):
     game = games[ctx.guild.id]
     try:
-        player = discord.utils.get(ctx.guild.members, display_name=nick)
+        players = [user for user in ctx.guild.members if nick == user.display_name or nick == str(user.id) or nick == user.name or nick == str(user)]
+
+        if len(players) == 1:
+            player = players[0]
+        else:
+            player = None
+            await ctx.send('There are multiple matches for the entered nickname. Please redo this command with their full discord name.')
 
         game.players.append(Player(player.id, ctx.channel.id, SalemRole[role.upper()]))
         save(ctx.guild.id)
@@ -148,7 +152,7 @@ async def add_player(ctx, nick, role):
         await ctx.send(f'{player.mention} Welcome! This is your own private channel. This is where you are told what your role is, and where you tell the host how you use it as well. On top of that, this is where you store your will. You must have it pinned, or it will not go through, in the case that you die. You have been given pin perms for this channel.')
         await ctx.message.delete()
     except:
-        await ctx.send(f'Role does not exist. To use a custom role, put CUSTOM for the role parameter. See list of roles here: https://drive.google.com/file/d/1MdQJCUaKRM_jPIPN2NU3IcY0Vtp2fnI3/view?usp=sharing')
+        await ctx.send(f'Role or user does not exist. To use a custom role, put CUSTOM for the role parameter. See list of roles here: https://drive.google.com/file/d/1MdQJCUaKRM_jPIPN2NU3IcY0Vtp2fnI3/view?usp=sharing')
 
 @bot.command(brief='Starts the game instance')
 @commands.has_permissions(administrator=True)
@@ -169,7 +173,8 @@ async def time(ctx):
 @bot.command(brief='Changes the transition times for the game instance')
 @commands.has_permissions(administrator=True)
 async def set_transition_times(ctx, hour1, minute1, hour2, minute2):
-    games[ctx.guild.id].transition_times = [[int(hour1), int(minute1)], [int(hour2), int(minute2)]]
+    games[ctx.guild.id].transition_times = [
+        [int(hour1), int(minute1)], [int(hour2), int(minute2)]]
     save(ctx.guild.id)
     await ctx.message.add_reaction('✅')
 
@@ -177,7 +182,15 @@ async def set_transition_times(ctx, hour1, minute1, hour2, minute2):
 @commands.has_permissions(administrator=True)
 async def set_role(ctx, nick, role):
     game = games[ctx.guild.id]
-    user = discord.utils.get(ctx.guild.members, display_name=nick)
+
+    players = [member for member in ctx.guild.members if nick == member.display_name or nick == str(member.id) or nick == member.name or nick == str(member)]
+
+    if len(players) == 1:
+        user = players[0]
+    else:
+        user = None
+        await ctx.send('There are multiple matches for the entered nickname. Please redo this command with their full discord name or id.')
+
     player = game.player_from_id(user.id)
     player.role = SalemRole[role]
     save(ctx.guild.id)
@@ -188,6 +201,15 @@ async def set_role(ctx, nick, role):
 @bot.command(aliases=['w', 'message', 'msg', 'pm', 'dm'], brief='Whispers to a player in the game and tells everyone in the game')
 async def whisper(ctx, nick, *, message):
     recipient = discord.utils.get(ctx.guild.members, display_name=nick)
+
+    players = [member for member in ctx.guild.members if nick == member.display_name or nick == str(member.id) or nick == member.name or nick == str(member)]
+
+    if len(players) == 1:
+        recipient = players[0]
+    else:
+        recipient = None
+        await ctx.send('There are multiple matches for the entered nickname. Please redo this command with their full discord name or id.')
+
     if ctx.guild.id in games and games[ctx.guild.id].day == int(games[ctx.guild.id].day) and recipient != ctx.author:
         await bot.get_channel(games[ctx.guild.id].player_from_id(recipient.id).personal_channel_id).send(f'**{ctx.author.display_name}** whispers *{message}*')
         await bot.get_channel(games[ctx.guild.id].game_channel_id).send(f'**{ctx.author.display_name}** is whispering to **{recipient.display_name}**')
@@ -202,7 +224,7 @@ async def list_players(ctx):
     game = games[ctx.guild.id]
     await ctx.send(f'**Players:**')
     for player in game.players:
-            await ctx.send(f'•{ctx.guild.get_member(player.user_id).display_name} ({bot.get_user(player.user_id)})')
+        await ctx.send(f'•{ctx.guild.get_member(player.user_id).display_name} ({bot.get_user(player.user_id)})')
 
 @bot.command(aliases=['ginfo', 'game_data', 'gdata', 'data'], brief='Lists info about the game')
 @commands.has_permissions(administrator=True)
@@ -217,7 +239,8 @@ async def game_info(ctx):
 
 def save(guild_id):
     game = games[guild_id]
-    data = [str(guild_id), str(game.in_progress), str(game.game_channel_id), str(game.player_role_id), str(game.transition_times[0][0]), str(game.transition_times[0][1]), str(game.transition_times[1][0]), str(game.transition_times[1][1]), str(game.day)]
+    data = [str(guild_id), str(game.in_progress), str(game.game_channel_id), str(game.player_role_id), str(game.transition_times[0][0]), str(
+        game.transition_times[0][1]), str(game.transition_times[1][0]), str(game.transition_times[1][1]), str(game.day)]
 
     for player in game.players:
         data.append(str(player.user_id))
@@ -229,12 +252,12 @@ def save(guild_id):
     if str(guild_id) in guild_ids:
         row = guild_ids.index(str(guild_id)) + 1
         sheet.delete_rows(row)
- 
+
     sheet.insert_row(data, row)
 
 def load(guild_id):
     data = sheet.col_values(1)
-    
+
     guild_ids = sheet.col_values(1)
     try:
         row = guild_ids.index(str(guild_id)) + 1
@@ -243,7 +266,8 @@ def load(guild_id):
         in_progress = bool(data[1])
         game_channel_id = int(data[2])
         player_role_id = int(data[3])
-        transition_times = [[int(data[4]), int(data[5])], [int(data[6]), int(data[7])]]
+        transition_times = [[int(data[4]), int(data[5])], [
+            int(data[6]), int(data[7])]]
         day = float(data[8])
 
         players = []
@@ -258,7 +282,7 @@ def load(guild_id):
         print('Loaded game of guild_id:', guild_id)
     except:
         print(f'Data for {guild_id} does not exist. Game instance not created.')
-    
+
 @bot.command(brief='Progress time manually. Only use if necessary.')
 @commands.has_permissions(administrator=True)
 async def progress_time(ctx):
@@ -276,9 +300,9 @@ async def delete_game(ctx):
             pinned_messages = await bot.get_channel(player.personal_channel_id).pins()
             for pin in pinned_messages:
                 await pin.unpin()
-        
+
         del games[ctx.guild.id]
-        
+
         guild_ids = sheet.col_values(1)
         row = guild_ids.index(str(ctx.guild.id)) + 1
         sheet.delete_rows(row)
